7c06e6eb176941cbc7b845b6ab83e166c50b7c7f
Nick Maslov <kategorychnost@gmail.com>
2021-11-15 17:17:50 -0500

    created class forms for create, update, delete; added django + bootstrap pagination, and info messages

--- cities/views.py
+++ cities/views.py
@@ -1,17 +1,29 @@
-from typing import Dict, Any
-
+from django.contrib import messages
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
 
+from .forms import HtmlForm, CityForm
 from .models import City
 
 __all__ = (
     'home',
     'CityDetailView',
+    'CityCreateView',
+    'CityUpdateView',
+    'CityDeleteView',
+    'CityListView',
 )
 
 
 def home(request, pk=None):
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
     # if pk:
     #     # city = City.objects.filter(id=pk).first()
     #     # city = City.objects.get(id=pk)
@@ -19,11 +31,55 @@ def home(request, pk=None):
     #
     #     context: dict[str, Any] = {'object': city}
     #     return render(request, 'cities/detail.html', context)
+    form = CityForm()
     cities = City.objects.all()
-    context: dict[str, Any] = {'objects_list': cities}
+    lst = Paginator(cities, 2)
+    page_number = request.GET.get('page')
+    page_obj = lst.get_page(page_number)
+    context = {'page_obj': page_obj, "form": form}
     return render(request, 'cities/home.html', context)
 
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(SuccessMessageMixin, CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
+    success_message = "City was created successfully"
+
+
+class CityUpdateView(SuccessMessageMixin,  UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
+    success_message = "City was updated successfully"
+
+
+class CityDeleteView(SuccessMessageMixin, DeleteView):
+    model = City
+    # template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, "City was deleted successfully")
+        return self.post(request, *args, **kwargs)
+
+
+
+class CityListView(ListView):
+    paginate_by = 2
+    template_name = 'cities/home.html'
+    model = City
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = CityForm()
+        context['form'] = form
+        return context
+
